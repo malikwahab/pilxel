ff015af0850e0728971d6957cf781695470455f8
Abdulmalik Abdulwahab <abdulmalik.abdulwahab@andela.com>
2016-09-08 11:39:17 +0100

    [Feature #129966499] setup django to server bundle files

--- django_photo_application/django_photo_application/settings.py
+++ django_photo_application/django_photo_application/settings.py
@@ -1,9 +1,7 @@
 """
 Django settings for django_photo_application project.
-
 For more information on this file, see
 https://docs.djangoproject.com/en/1.7/topics/settings/
-
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.7/ref/settings/
 """
--- django_photo_application/pilxel/views.py
+++ django_photo_application/pilxel/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
-
+from django.views.generic import TemplateView
 # Create your views here.
+
+
+class IndexView(TemplateView):
+
+    template_name = "index.html"
