4175b8db07431fad83c0342ab2feff6732e2dbd8
Abdulmalik Abdulwahab <abdulmalik.abdulwahab@andela.com>
2016-09-13 09:15:25 +0100

    [Feature #129952021] write test for Folder model

--- django_photo_application/pilxel/tests/test_models.py
+++ django_photo_application/pilxel/tests/test_models.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User
 from django.test import TestCase
 
-from pilxel.models import ImageModel
+from pilxel.models import FolderModel, ImageModel
 
 
 class ImageModelTestCase(TestCase):
@@ -18,3 +18,19 @@ class ImageModelTestCase(TestCase):
         self.assertEqual(image_object.owner, user)
         self.assertEqual(image_object.name, "Adventure")
         self.assertEqual(ImageModel.objects.count(), 1)
+
+
+class FolderModelTestCase(TestCase):
+    """Tests for the Folder model"""
+
+    def setUp(self):
+        user = User.objects.create(
+            username='malikwahab', password='malikwahab')
+        FolderModel.objects.create(name='Adventure', owner=user)
+
+    def test_image_model(self):
+        user = User.objects.all()[0]
+        folder_object = FolderModel.objects.all()[0]
+        self.assertEqual(folder_object.owner, user)
+        self.assertEqual(folder_object.name, "Adventure")
+        self.assertEqual(FolderModel.objects.count(), 1)
