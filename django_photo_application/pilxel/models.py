03cd64d6649a005059f78e6ca74712d449be640e
Abdulmalik Abdulwahab <abdulmalik.abdulwahab@andela.com>
2016-09-13 12:07:51 +0100

    [Feature #129952589] creaste base model

--- django_photo_application/pilxel/models.py
+++ django_photo_application/pilxel/models.py
@@ -1,3 +1,22 @@
+from __future__ import unicode_literals
+
 from django.db import models
 
+
 # Create your models here.
+
+
+def upload_directory_path(instance, filename):
+    # file will be uploaded to MEDIA_ROOT/images/user_<id>
+    return 'images/user_{0}_{1}/'.format(instance.owner.id, filename)
+
+
+class BaseModel(models.Model):
+
+    name = models.CharField(max_length=100, unique=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('date_modified',)
+        abstract = True
