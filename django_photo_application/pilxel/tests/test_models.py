e4ae418545d015326e5dde2c9b9a8a0e420471c6
Abdulmalik Abdulwahab <abdulmalik.abdulwahab@andela.com>
2016-09-13 09:11:48 +0100

    [Feature #129952021] write test for Image model

---
+++ django_photo_application/pilxel/tests/test_models.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.models import User
+from django.test import TestCase
+
+from pilxel.models import ImageModel
+
+
+class ImageModelTestCase(TestCase):
+    """Tests for the Image model"""
+
+    def setUp(self):
+        user = User.objects.create(
+            username='malikwahab', password='malikwahab')
+        ImageModel.objects.create(name='Adventure', owner=user)
+
+    def test_image_model(self):
+        user = User.objects.all()[0]
+        image_object = ImageModel.objects.all()[0]
+        self.assertEqual(image_object.owner, user)
+        self.assertEqual(image_object.name, "Adventure")
+        self.assertEqual(ImageModel.objects.count(), 1)
